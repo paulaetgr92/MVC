e415cf1420fe7c021f72e27057beb83213f3855f
paulaetgr92 <paulaetgr92@gmail.com>
2025-04-07 15:24:30 -0300

    Removendo pasta flask API do repositório

--- OneDrive/Área de Trabalho/flask API/turma.py
+++
@@ -1,9 +0,0 @@
-class Turma:
-    def __init__(self, id, professor_id, alunos, nome):
-        self.id = id
-        self.professor_id = professor_id
-        self.alunos = alunos  # Lista de IDs de alunos
-        self.nome = nome
-
-    def to_dict(self):
-        return {"id": self.id, "professor_id": self.professor_id, "alunos": self.alunos, "nome": self.nome}
